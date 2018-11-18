d1de0566868409a1e59e9a7540037dc1bb056c9c
Hung912 <hunghusthedspi@gmail.com>
2018-11-18 22:03:02 +0700

    fix birthdate

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,13 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :validatable
 
   enum role: [:user, :admin, :vip]
-  after_initialize :set_default_role, :if => :new_record?
+  after_initialize :set_default_role, :set_default_info, :if => :new_record?
+
+  def set_default_info
+    self.birthdate ||= '1996-1-1'
+    self.sex ||= 'male'
+    self.phone ||= '01234568'
+  end
 
   def set_default_role
     self.user_role ||= :user
